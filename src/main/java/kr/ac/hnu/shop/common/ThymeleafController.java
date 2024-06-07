2c662e13cee40e01ab56b283b77196a546b7644c
chopperkim <chocojhkim@gmail.com>
2024-06-07 20:52:57 +0900

    thymeleaf 2

--- src/main/java/kr/ac/hnu/shop/common/ThymeleafController.java
+++ src/main/java/kr/ac/hnu/shop/common/ThymeleafController.java
@@ -4,10 +4,12 @@ import kr.ac.hnu.shop.item.ItemDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/thymeleaf")
 @Controller
@@ -39,7 +41,48 @@ public class ThymeleafController {
     }
 
     @GetMapping("/ex03")
-    public String ex03(Model model) {
+    public String ex03() {
         return "thymeleaf/ex03";
     }
+
+    @GetMapping("/ex04")
+    public String ex04() {
+        return "thymeleaf/ex04";
+    }
+
+    @GetMapping("/ex05")
+    public String ex05() {
+        return "thymeleaf/ex05";
+    }
+
+    @GetMapping("/ex06")
+    public String ex06(String name, int age, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "thymeleaf/ex06";
+    }
+
+    @GetMapping("/ex07")
+    public String ex07() {
+        return "thymeleaf/ex07";
+    }
+
+    // @ModelAttribute
+    // 부착 위치: 파라미터, 메소드
+    // 내부적으로 model.addAttribute("items", list);
+    @ModelAttribute("items")
+    public List<ItemDTO> items() {
+        List<ItemDTO> list = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            ItemDTO item = ItemDTO.builder()
+                    .name("테스트 상품" + i)
+                    .price(10000 * i)
+                    .description("테스트 상품 상세 설명" + i)
+                    .registerDate(LocalDateTime.now())
+                    .build();
+            list.add(item);
+        }
+        return list;
+    }
+
 }
