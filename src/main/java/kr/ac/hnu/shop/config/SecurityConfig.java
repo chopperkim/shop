041cfad140ef7ae5275827454f7a922568d93154
chopperkim <chocojhkim@gmail.com>
2024-06-12 19:01:18 +0900

    shop h2 db 파일

--- src/main/java/kr/ac/hnu/shop/config/SecurityConfig.java
+++ src/main/java/kr/ac/hnu/shop/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package kr.ac.hnu.shop.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -39,5 +41,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer configureH2ConsoleEnable() {
+        return s -> s.ignoring().requestMatchers(PathRequest.toH2Console());
     }
 }
